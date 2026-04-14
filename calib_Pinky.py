4ff4aef1053b2bb0a196edbac772c37c766eacbf
Sarah <98198971+Sarah-Rani@users.noreply.github.com>
2026-04-14 11:18:18 -0700

    Update values in calib_Pinky.py
    changed R_BIAS = 433, let R_WIRE = 113 (though this ranged from 113 to 130)

--- calib_Pinky.py
+++ calib_Pinky.py
@@ -9,19 +9,19 @@ def calib_pinky():
 	calib = {}
 	calib["R_SH"] = 0.0030    #Ohms (at 4K, different chip) #same as SK
 
-	calib["M_FB"] = np.array([-24.99]*16)    #same as SK
-	calib["M_FB"][2] = -calib["M_FB"][2]
+	calib["M_FB"] = np.array([-8.0]*16)    #same as SK
+	calib["M_FB"][2] = -calib["M_FB"][2] #is this ever used?
 	
 	calib["R_WIRE"] = np.array([113.00]*16)    #ohms include in R_FB and R_BIAS; modified
 	
-	calib["R_BIAS"] = np.array([467.00]*16)
+	calib["R_BIAS"] = np.array([433.00]*16)
 	
 	calib["V_B_MAX"] = np.array([2.500, 2.500, 2.500, 2.500, 
 						2.500, 2.500, 2.500, 2.500, 
 						2.500, 2.500, 2.500, 2.500, 
 						2.500, 2.500, 2.500, 2.500])
 	
-	calib["R_FB"] = np.array([5000.00]*16)
+	calib["R_FB"] = np.array([15000.00]*16)
 	
 	calib["V_FB_MAX"] =  2*np.array([0.975]*16) #V (I think??)
 	
